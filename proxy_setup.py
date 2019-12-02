8875ccce03b11f57dcc22eff8554978cc20dc00a
Paul Louis <iampaullouis@gmail.com>
2019-12-02 16:03:18 +0530

    Add proxy file

---
+++ proxy_setup.py
@@ -0,0 +1,57 @@
+
+PROXY_HOST = 'zproxy.lum-superproxy.io'  # rotating proxy or host
+PROXY_PORT = 22225  # port
+PROXY_USER = 'lum-customer-clootrack-zone-suyog_india-ip-158.46.135.111'  # username
+PROXY_PASS = '3ouqoo9lsm77'  # password
+
+manifest_json = """
+{
+    "version": "1.0.0",
+    "manifest_version": 2,
+    "name": "Chrome Proxy",
+    "permissions": [
+        "proxy",
+        "tabs",
+        "unlimitedStorage",
+        "storage",
+        "<all_urls>",
+        "webRequest",
+        "webRequestBlocking"
+    ],
+    "background": {
+        "scripts": ["background.js"]
+    },
+    "minimum_chrome_version":"22.0.0"
+}
+"""
+
+background_js = """
+var config = {
+        mode: "fixed_servers",
+        rules: {
+        singleProxy: {
+            scheme: "http",
+            host: "%s",
+            port: parseInt(%s)
+        },
+        bypassList: ["localhost"]
+        }
+    };
+
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "%s",
+            password: "%s"
+        }
+    };
+}
+
+chrome.webRequest.onAuthRequired.addListener(
+            callbackFn,
+            {urls: ["<all_urls>"]},
+            ['blocking']
+);
+""" % (PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS)
