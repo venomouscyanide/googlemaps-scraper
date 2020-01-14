d697dbb377a63074aecea0b605f0abfeb392ab44
Paul Louis <paul.louis@clootrack.com>
2020-01-14 10:18:39 +0530

    Update doc, increase review default limit

--- scraper.py
+++ scraper.py
@@ -2,8 +2,8 @@
 
 """
     usage: python3 scraper.py --N 100
-    where N is the number of reviews to be scraped. Scroll limit is by default 160. Scraper stop when scroll limit
-    OR number of reviews is reached.
+    where N is the number of reviews to be scraped. Scraper stop when number of reviews is reached OR when the stack contains the
+    same number of reviews scraped(meaning no loading of reviews has happened for 20 clicks. The stack size by default is 20).
 
     Input file is by default urls.txt where the N number of URL's are separated by | (piped)
     Output files to be written to are configured using config.json
@@ -14,7 +14,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Google Maps reviews scraper.')
-    parser.add_argument('--N', type=int, default=100, help='Number of reviews to scrape')
+    parser.add_argument('--N', type=int, default=1500, help='Number of reviews to scrape')
     parser.add_argument('--i', type=str, default='urls.txt', help='target URLs file')
 
     args = parser.parse_args()
